4a63915fa0aa95c6deba02be881f85c1df0d738a
Lucas Alonso <lucasalonso11800@gmail.com>
2021-04-13 16:21:55 -0300

    Completadas listas de jugadores

--- src/components/Jugadores.js
+++ src/components/Jugadores.js
@@ -1,3 +1,38 @@
+//IMPORTAR CARAS
+import Alvarez from '../assets/caras/river/Alvarez.jpg';
+import Angileri from '../assets/caras/river/Angileri.jpg';
+import Bologna from '../assets/caras/river/Bologna.jpg';
+import Borre from '../assets/caras/river/Borre.jpg';
+import Carrascal from '../assets/caras/river/Carrascal.jpg';
+import Casco from '../assets/caras/river/Casco.jpg';
+import DeLaCruz from '../assets/caras/river/DeLaCruz.jpg';
+import Diaz from '../assets/caras/river/Diaz.jpg';
+import Enzo from '../assets/caras/river/Enzo.jpg';
+import Girotti from '../assets/caras/river/Girotti.jpg';
+import Maidana from '../assets/caras/river/Maidana.jpg';
+import Martinez from '../assets/caras/river/Martinez.jpg';
+import Palavecino from '../assets/caras/river/Palavecino.jpg';
+import Paradela from '../assets/caras/river/Paradela.jpg';
+import Ponzio from '../assets/caras/river/Ponzio.jpg';
+import Rojas from '../assets/caras/river/Rojas.jpg';
+import Suarez from '../assets/caras/river/Suarez.jpg';
+import Vigo from '../assets/caras/river/Vigo.jpg';
+import Zuculini from '../assets/caras/river/Zuculini.jpg';
+
+
+import Cardona from '../assets/caras/boca/Cardona.jpg';
+import Izquierdoz from '../assets/caras/boca/Izquierdoz.jpg';
+import Maroni from '../assets/caras/boca/Maroni.jpg';
+import Medina from '../assets/caras/boca/Medina.jpg';
+import Rojo from '../assets/caras/boca/Rojo.jpg';
+import Rossi from '../assets/caras/boca/Rossi.jpg';
+import Soldano from '../assets/caras/boca/Soldano.jpg';
+import Varela from '../assets/caras/boca/Varela.jpg';
+import Zambrano from '../assets/caras/boca/Zambrano.jpg';
+import Zeballos from '../assets/caras/boca/Zeballos.jpg';
+
+// JUGADORES
+
 export const jugadoresSeleccion = [
     {
         nombre: 'Armani',
@@ -5,8 +40,8 @@ export const jugadoresSeleccion = [
         id: 1
     },
     {
-        nombre: 'Marchesin',
-        imagen: 'https://images.ole.com.ar/collections/avatares/59891.jpg',
+        nombre: 'Andrada',
+        imagen: 'https://images.ole.com.ar/collections/avatares/86266.jpg',
         id: 2
     },
     {
@@ -20,8 +55,8 @@ export const jugadoresSeleccion = [
         id: 4
     },
     {
-        nombre: 'Funes Mori',
-        imagen: 'https://images.ole.com.ar/collections/avatares/121221.jpg',
+        nombre: 'Montiel',
+        imagen: 'https://images.ole.com.ar/collections/avatares/218430.jpg',
         id: 5
     },
     {
@@ -113,26 +148,216 @@ export const jugadoresRiver = [
         id: 1
     },
     {
-        nombre: 'Marchesin',
-        imagen: 'https://images.ole.com.ar/collections/avatares/59891.jpg',
+        nombre: 'Bologna',
+        imagen: Bologna,
         id: 2
     },
     {
-        nombre: 'Musso',
-        imagen: 'https://images.ole.com.ar/collections/avatares/121537.jpg',
+        nombre: 'Diaz',
+        imagen: Diaz,
         id: 3
+    },
+    {
+        nombre: 'Rojas',
+        imagen: Rojas,
+        id: 4
+    },
+    {
+        nombre: 'Casco',
+        imagen: Casco,
+        id: 5
+    },
+    {
+        nombre: 'Angileri',
+        imagen: Angileri,
+        id: 6
+    },
+    {
+        nombre: 'Vigo',
+        imagen: Vigo,
+        id: 7
+    },
+    {
+        nombre: 'Martinez',
+        imagen: Martinez,
+        id: 8
+    },
+    {
+        nombre: 'Maidana',
+        imagen: Maidana,
+        id: 9
+    },
+    {
+        nombre: 'Montiel',
+        imagen: 'https://images.ole.com.ar/collections/avatares/218430.jpg',
+        id: 10
+    },
+    {
+        nombre: 'Zuculini',
+        imagen: Zuculini,
+        id: 11
+    },
+    {
+        nombre: 'Ponzio',
+        imagen: Ponzio,
+        id: 12
+    },
+    {
+        nombre: 'Enzo',
+        imagen: Enzo,
+        id: 13
+    },
+    {
+        nombre: 'Palavecino',
+        imagen: Palavecino,
+        id: 14
+    },
+    {
+        nombre: 'Paradela',
+        imagen: Paradela,
+        id: 15
+    },
+    {
+        nombre: 'De la Cruz',
+        imagen: DeLaCruz,
+        id: 16
+    },
+    {
+        nombre: 'Carrascal',
+        imagen: Carrascal,
+        id: 17
+    },
+    {
+        nombre: 'Suarez',
+        imagen: Suarez,
+        id: 18
+    },
+    {
+        nombre: 'Alvarez',
+        imagen: Alvarez,
+        id: 19
+    },
+    {
+        nombre: 'Girotti',
+        imagen: Girotti,
+        id: 20
+    },
+    {
+        nombre: 'Borre',
+        imagen: Borre,
+        id: 21
     },
 ];
 
 export const jugadoresBoca = [
     {
-        nombre: 'Armani',
-        imagen: 'https://images.ole.com.ar/collections/avatares/149575.jpg',
+        nombre: 'Andrada',
+        imagen: 'https://images.ole.com.ar/collections/avatares/86266.jpg',
         id: 1
     },
     {
-        nombre: 'Marchesin',
-        imagen: 'https://images.ole.com.ar/collections/avatares/59891.jpg',
+        nombre: 'Rossi',
+        imagen: Rossi,
         id: 2
+    },
+    {
+        nombre: 'Lopez',
+        imagen: 'https://images.ole.com.ar/collections/avatares/76734.jpg',
+        id: 3
+    },
+    {
+        nombre: 'Izquierdoz',
+        imagen: Izquierdoz,
+        id: 4
+    },
+    {
+        nombre: 'Rojo',
+        imagen: Rojo,
+        id: 5
+    },
+    {
+        nombre: 'Zambrano',
+        imagen: Zambrano,
+        id: 6
+    },
+    {
+        nombre: 'Mas',
+        imagen: 'https://images.ole.com.ar/collections/avatares/76734.jpg',
+        id: 7
+    },
+    {
+        nombre: 'Buffarini',
+        imagen: 'https://images.ole.com.ar/collections/avatares/113808.jpg',
+        id: 8
+    },
+    {
+        nombre: 'Capaldo',
+        imagen: 'https://images.ole.com.ar/collections/avatares/474094.jpg',
+        id: 9
+    },
+    {
+        nombre: 'Fabra',
+        imagen: 'https://images.ole.com.ar/collections/avatares/150427.jpg',
+        id: 10
+    },
+    {
+        nombre: 'Campuzano',
+        imagen: 'https://images.ole.com.ar/collections/avatares/444359.jpg',
+        id: 11
+    },
+    {
+        nombre: 'Obando',
+        imagen: 'https://images.ole.com.ar/collections/avatares/442769.jpg',
+        id: 12
+    },
+    {
+        nombre: 'Medina',
+        imagen: Medina,
+        id: 13
+    },
+    {
+        nombre: 'Maroni',
+        imagen: Maroni,
+        id: 14
+    },
+    {
+        nombre: 'Zeballos',
+        imagen: Zeballos,
+        id: 16
+    },
+    {
+        nombre: 'Varela',
+        imagen: Varela,
+        id: 17
+    },
+    {
+        nombre: 'Cardona',
+        imagen: Cardona,
+        id: 18
+    },
+    {
+        nombre: 'Tevez',
+        imagen: 'https://images.ole.com.ar/collections/avatares/20312.jpg',
+        id: 19
+    },
+    {
+        nombre: 'Salvio',
+        imagen: 'https://images.ole.com.ar/collections/avatares/56492.jpg',
+        id: 20
+    },
+    {
+        nombre: 'Zarate',
+        imagen: 'https://images.ole.com.ar/collections/avatares/49696.jpg',
+        id: 21
+    },
+    {
+        nombre: 'Villa',
+        imagen: 'https://images.ole.com.ar/collections/avatares/226896.jpg',
+        id: 22
+    },
+    {
+        nombre: 'Soldano',
+        imagen: Soldano,
+        id: 23
     }
 ];
